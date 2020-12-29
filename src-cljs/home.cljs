a34632ba90106bfd0eebae0bcd5e9392fef7098a
veschin <unclemadolu2@gmail.com>
2020-12-29 22:23:03 +0700

    home: fixed bugs. created new atoms. added post queries

--- src-cljs/home.cljs
+++ src-cljs/home.cljs
@@ -1,7 +1,7 @@
 (ns home
   (:require [reagent.core :as reagent]
-            [ajax.core :refer [GET]]
-            [clojure.string :refer [join]]
+            [ajax.core :refer [GET POST]]
+            [clojure.string :refer [join trim]]
             [manager.application.pages :refer [resolve-page]]
             [manager.application.router :as r]
             [manager.application.state :refer [set-page!]]
@@ -10,7 +10,7 @@
 
 (defonce ref'state nil)
 (def ref'storage (reagent/atom {}))
-(def disabled? (reagent/atom true))
+(def disabled? (reagent/atom false))
 (def saved? (reagent/atom 0))
 
 (defn- create-margin
@@ -36,11 +36,14 @@
         phone-val (->> (.-value phone) (re-seq #"\d") vec)
         updated-phone (update-subvecs phone-val [[0 1] [1 4] [4 7] [7 9] [9 11]] "-")]
     (when (valid? phone-val)
+      (swap! ref'storage assoc :phone updated-phone)
       (set* phone updated-phone))))
 
 (defn- update-name [field]
-  (let [name (select (str "#" field))]
-    (set* name (-> name .-value clojure.string/capitalize))))
+  (let [name (select (str "#" field))
+        updated-name (-> name .-value clojure.string/capitalize)]
+    (swap! ref'storage assoc (keyword field) updated-name)
+    (set* name updated-name)))
 
 
 (defn- update-birth-date []
@@ -49,17 +52,18 @@
         date-val  (->> (.-value date) (re-seq #"\d") vec)
         updated-date (update-subvecs date-val [[0 2] [2 4] [4 8]] ".")]
     (when (valid? date-val)
+      (swap! ref'storage assoc :birth-date updated-date)
       (set* date updated-date))))
 
 (defn- inputs []
   (let [fields [:first-name :patronomyc :last-name
-                :age :birth-date :weigth :heigth :phone :foot-size]
+                :age :birth-date :weight :height :phone :foot-size]
         placeholders {:first-name "Имя"
                       :patronomyc "Отчество"
                       :last-name "Фамилия"
                       :age "Возраст"
-                      :weigth "Вес"
-                      :heigth "Рост"
+                      :weight "Вес"
+                      :height "Рост"
                       :phone "Телефон"
                       :birth-date "Дата рождения"
                       :foot-size "Размер обуви"}]
@@ -74,20 +78,27 @@
           :disabled @disabled?
           :placeholder (field placeholders)
           :on-change (fn [_ ev]
-                       (let [value (.-value ev)]
+                       (let [value (-> ev .-value trim)]
+                         (swap! ref'storage assoc field value)
+                         (prn field value)
                          (case field
                            :phone (update-phone)
-
                            :birth-date (update-birth-date)
                            :first-name (update-name 'first-name)
                            :patronomyc (update-name 'patronomyc)
-                           :last-name (update-name 'last-name))
-                         (swap! ref'storage assoc field value)))}]]))))
+                           :last-name (update-name 'last-name))))}]]))))
+
+(defn- save-page []
+  (POST "/save-page/"
+    {:params @ref'storage
+     :format :json}))
 
 (defn- medical-section []
   (let [insoles ["Junior" "J2" "S" "M" "L" "XL" "XXL"]
+        insole-value #(-> % (js->clj :keywordize-keys true) :value)
         disabled-color (reagent/atom (when (not @disabled?) "#D0EA2B"))
         date (subs (js/Date) 4 15)
+        _ (swap! ref'storage assoc :appointment-date (js/Date.))
         save-date (when (-> @saved? zero? not)
                     [:p {:style (create-margin 'top 15)} (str "Сохранение номер " @saved? " от " date)])]
     [semantic/segment
@@ -98,20 +109,23 @@
       [semantic/dropdown {:placeholder "Выбор стельки"
                           :selection true
                           :disabled @disabled?
+                          :on-change  #(swap! ref'storage assoc :insole (insole-value %2))
                           :options (map (fn [val] {:key val :value val :text val}) insoles)}]]
-
      [semantic/grid-row
       {:style (create-margin 10)}
       [semantic/textarea {:style {:padding 10
                                   :width 300
                                   :height 100}
                           :disabled @disabled?
+                          :on-change #(swap! ref'storage assoc :anamnesis (.-value %2))
+
                           :placeholder "Анамнез"}]]
      [semantic/grid-row
       {:style (create-margin 10)}
       [semantic/textarea {:style {:padding 10
                                   :width 300
                                   :height 100}
+                          :on-change #(swap! ref'storage assoc :recommendation (.-value %2))
                           :disabled @disabled?
                           :placeholder "Врачебная рекомендация"}]]
      [semantic/grid-row
@@ -123,17 +137,22 @@
                               :color @disabled-color}
                       :name "circle" :size "large"}]
       [semantic/grid-row
-       [semantic/button {:on-click #(swap! saved? inc)} "Сохранить документ"]
+       [semantic/button {:on-click #((do
+                                       (save-page)
+                                       (swap! saved? inc)))} "Сохранить документ"]
        save-date]]]))
 
-(def images (reagent/atom []))
+(def images (reagent/atom {}))
+(def images-counter (reagent/atom nil))
 
 (defn render-image [file-added-event]
   (let [file (-> (.. file-added-event -target -files) array-seq first)
         file-reader (js/FileReader.)]
-    (set! (.-onload file-reader) #(swap! images conj (-> % .-target .-result)))
+    (prn file)
+    (set! (.-onload file-reader) #(do
+                                    (swap! images assoc (swap! images-counter inc) (-> % .-target .-result))
+                                    (swap! ref'storage assoc :images (vals @images))))
     (.readAsDataURL file-reader file)))
-
 
 (defn- images-section []
   (into
@@ -148,9 +167,10 @@
                       :multiple true
                       :on-change render-image
                       :disabled @disabled?}]]]
-   (for [img @images]
+   (for [[key img] @images]
      [semantic/image {:src img
                       :centered true
+                      :on-click (fn [_] (swap! images dissoc key))
                       :className "rounded"
                       :style {:width 350
                               :height 350}}])))
