15ddfd683139ef7d2b117f9750c50d522c90d643
veschin <unclemadolu2@gmail.com>
2021-01-06 23:19:13 +0700

    db: removed indication table

--- src/db.clj
+++ src/db.clj
@@ -17,32 +17,23 @@
                [:age :int]
                [:weight :int]
                [:height :int]
-               [:foot_size :float]])
-
-(def indication [[:id :integer :primary :key :autoincrement]
-                 [:recommendation :text]
-                 [:anamnesis :text]
-                 [:images :blob]
-                 [:insole :text]
-                 [:patient_id :integer]
-                 [:appointment_date :date]
-                 ["FOREIGN KEY(patient_id) REFERENCES patients(id)"]])
-
+               [:foot_size :float]
+               [:recommendation :text]
+               [:anamnesis :text]
+               [:images :blob]
+               [:insole :text]
+               [:appointment_date :date]])
 
 (comment
   "init db"
   (do
     (sh "rm" "db/database.db")
     (jdbc/db-do-commands db-spec
-                         [(jdbc/create-table-ddl :patients patients)
-                          (jdbc/create-table-ddl :indication indication)]))
+                         (jdbc/create-table-ddl :patients patients)))
   "select all from patients"
   (jdbc/query db-spec (sql/format {:select [:*]
                                    :from [:patients]}))
-  (jdbc/query db-spec (sql/format {:select [:*]
-                                   :from [:indication]}))
-
-
+  (sh "sqlite3" "db/database.db" ".schema")
 
   ;
   )
